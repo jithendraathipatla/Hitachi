2a5fdee54c2591e3c9723bde531f88a251b235a0
Jithendra Kumar <jithendra.kumar@in.abb.com>
2021-04-02 12:49:24 +0530

    final commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#d4af37`,
         theme_color: `#d4af37`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
